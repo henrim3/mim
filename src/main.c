624565a82ec8cc0cb9592609858dc1faacf8bf54
Matti Henriksen <112562805+henrim3@users.noreply.github.com>
2025-04-07 01:31:04 -0400

    A rectangle

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 #define SCREEN_HEIGHT 480
 
 void scc(int code) {
-    if (code < 0) {
+    if (!code) {
         fprintf(stderr, "SDL ERROR: %s\n", SDL_GetError());
         exit(1);
     }
@@ -18,30 +18,43 @@ void* scp(void* ptr) {
     }
 }
 
+void drawThing() {}
+
 int main(int argc, char* argv[]) {
     printf("Started mim!\n");
 
     SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
     SDL_Surface* screenSurface = NULL;
-    SDL_Event event;
 
     scc(SDL_Init(SDL_INIT_VIDEO));
 
-    window = scp(SDL_CreateWindow("mim", SCREEN_WIDTH, SCREEN_HEIGHT, 0));
+    scc(SDL_CreateWindowAndRenderer("mim", SCREEN_WIDTH, SCREEN_HEIGHT, 0,
+                                    &window, &renderer));
 
     scc(SDL_ShowWindow(window));
 
-    screenSurface = SDL_GetWindowSurface(window);
+    SDL_FRect rect = {.x = 5, .y = 5, .w = 5, .h = 5};
 
-    // Main loop
+    SDL_Event event;
     while (1) {
-        if (SDL_PollEvent(&event) && event.type == SDL_EVENT_QUIT) break;
+        if (SDL_PollEvent(&event) && event.type == SDL_EVENT_QUIT) {
+            break;
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+        SDL_RenderClear(renderer);
+
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+        SDL_RenderRect(renderer, &rect);
+
+        SDL_RenderPresent(renderer);
     }
 
     printf("Closing :(\n");
 
-    SDL_UpdateWindowSurface(window);
     SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
     SDL_Quit();
 
     return 0;
